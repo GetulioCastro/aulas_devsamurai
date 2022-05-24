96530513a458d8700e5603598b889e9d18391e3b
Getúlio Augusto Ferreira de Castro Júnior <48964182+GetulioCastro@users.noreply.github.com>
2022-05-23 22:46:38 -0300

    ultimas aulas EC6 e configurações @babel

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { soma } from "./matematica";
+
+//class ListaConvidados {
+//  constructor() {
+//    this.convidados = [];
+//  }
+
+//  adicionar(nome, idade, cpf) {
+//    this.convidados.push({ nome: nome, idade: idade, cpf: cpf });
+//  }
+
+//  listarConvidados() {
+//    console.log(this.convidados);
+//  }
+//}
+
+//var listaConv = new ListaConvidados();
+
+//document.getElementById("adicionar").onclick = function() {
+//  var nome = document.getElementById("nome").value;
+//  var idade = document.getElementById("idade").value;
+//  var cpf = document.getElementById("cpf").value;
+//  listaConv.adicionar(nome, idade, cpf);
+//};
+
+//document.getElementById("listar").onclick = function() {
+//  listaConv.listarConvidados();
+//};
+
+const result = soma(1, 2);
+
+alert(result)
+
